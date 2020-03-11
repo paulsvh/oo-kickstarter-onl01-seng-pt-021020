ea93e3699e8ab1d35bfc252c0477335732f32758
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-11 00:27:55 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,7 @@ class Project
 
   def add_backer(backer) #takes in a backer instance (and project name) and adds the backer to its @backed_projects attribute.
     @backers << backer
+    backer.backed_projects << self
   end
 
 
